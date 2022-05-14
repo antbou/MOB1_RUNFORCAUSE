531ced478585704a21b4afed8be28b4f82e83aad
Anthony Bouillant <anthony.bouillant@cpnv.ch>
2022-05-14 15:37:52 +0200

    :art: Improve structure of Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
 import { Text, StyleSheet } from 'react-native';
 
-
-class Header extends Component<any> {
-
+/**
+ * @class Header
+ * @extends {Component<any, any>}
+ * @description This class is used to display the header of the application.
+ */
+export default class Header extends Component<any> {
     render() {
         return (
             <Text style={styles.header}> {this.props.children} </Text>
         );
     }
 }
-
-export default Header;
 
 const styles = StyleSheet.create({
     header: {
