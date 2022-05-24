94e9a210eeda446203f5c38030c4ab1dc250563a
Anthony <anthony.bouillant@cpnv.ch>
2022-05-24 18:34:19 +0200

    :coffin: Remove SignUpScreen

--- src/navigation/AuthStack.tsx
+++ src/navigation/AuthStack.tsx
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { Text, View, TextInput, StyleSheet, Pressable, Button } from 'react-native';
 import SignInScreen from '../screens/SignInScreen';
-import SignUpScreen from '../screens/SignUpScreen';
 
 export default class AuthStack extends Component {
 
@@ -12,9 +10,6 @@ export default class AuthStack extends Component {
         return (
             <this.Stack.Navigator>
                 <this.Stack.Screen name="SignIn" component={SignInScreen} options={{ title: 'Se connecter' }} />
-                <this.Stack.Screen name="SignUp" component={SignUpScreen} options={{
-                    title: 'S\'inscrire'
-                }} />
             </this.Stack.Navigator>
         );
     }
