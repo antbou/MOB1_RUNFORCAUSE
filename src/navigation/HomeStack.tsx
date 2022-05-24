96bdf4b6e9788015d3284dc3396aa18c6f2607cf
Anthony <anthony.bouillant@cpnv.ch>
2022-05-24 22:04:12 +0200

    :lipstick: Add icons in tab navigation

--- src/navigation/HomeStack.tsx
+++ src/navigation/HomeStack.tsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import ProfileScreen from '../screens/ProfileScreen';
 import TrackingScreen from '../screens/TrackingScreen';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 export default class HomeStack extends Component {
     constructor(props: any) {
@@ -12,9 +13,13 @@ export default class HomeStack extends Component {
 
     render() {
         return (
-            <this.Tab.Navigator initialRouteName='Profile' >
-                <this.Tab.Screen name="Profile" component={ProfileScreen} />
-                <this.Tab.Screen name="Tracking" component={TrackingScreen} />
+            <this.Tab.Navigator initialRouteName='Profile'>
+                <this.Tab.Screen name="Profile" component={ProfileScreen} options={{
+                    tabBarIcon: ({ size, color }) => (<Icon name={"rocket"} color={color} size={size} />)
+                }} />
+                <this.Tab.Screen name="Tracking" component={TrackingScreen} options={{
+                    tabBarIcon: ({ size, color }) => (<Icon name={"map"} color={color} size={size} />)
+                }} />
             </this.Tab.Navigator>
         );
     }
