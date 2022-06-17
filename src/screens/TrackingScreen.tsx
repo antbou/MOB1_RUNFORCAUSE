bf1ac7f9790fd4a87fac1365d331b2f8b56f111c
Anthony Bouillant <anthony.bouillant@cpnv.ch>
2022-06-17 14:25:07 +0200

    :art: Add map in TrackingScreen

--- src/screens/TrackingScreen.tsx
+++ src/screens/TrackingScreen.tsx
@@ -1,22 +1,61 @@
-import React, { Component } from "react";
-import { Text, View, StyleSheet } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
 
-export default class TrackingScreen extends Component {
-    render() {
-        return (
-            <SafeAreaView style={styles.SafeAreaView}>
-                <View style={{ flex: 1 }}>
-                    <Text>TrackingScreen</Text>
-                </View>
-            </SafeAreaView>
-        );
-    }
+import React, { useEffect, useState } from 'react';
+import { AuthContext } from '../navigation/AuthProvider';
+import config from '../config/config.json';
+import * as Location from 'expo-location';
+import { Dimensions, SafeAreaView } from 'react-native';
+import MapView, { Marker, Region } from 'react-native-maps';
+import { LocationGeocodedAddress, LocationObject } from 'expo-location';
+
+export default function TrackingScreen() {
+    const context = React.useContext(AuthContext);
+    const [address, setAddress] = useState<LocationGeocodedAddress | null>(null);
+    const [errorMsg, setErrorMsg] = useState('');
+    const [region, setRegion] = useState({
+        latitude: 0,
+        longitude: 0,
+        latitudeDelta: 0.01,
+        longitudeDelta: 0.01,
+    });
+
+    useEffect(() => {
+        (async () => {
+            let { status } = await Location.requestForegroundPermissionsAsync();
+            if (status !== 'granted') {
+                setErrorMsg('Permission to access location was denied');
+                return;
+            }
+            let location = await Location.getCurrentPositionAsync({});
+            setRegion({
+                latitude: location.coords.latitude,
+                longitude: location.coords.longitude,
+                latitudeDelta: 0.01,
+                longitudeDelta: 0.01,
+            });
+        })();
+    }, []);
+
+    return (
+        <SafeAreaView >
+            <MapView
+                style={{
+                    width: Dimensions.get('window').width,
+                    height: Dimensions.get('window').height
+                }}
+                region={{
+                    latitude: region.latitude ?? 0,
+                    longitude: region.longitude ?? 0,
+                    latitudeDelta: 0.0922,
+                    longitudeDelta: 0.0421
+                }}
+                onRegionChangeComplete={(region) => setRegion(region)}
+            >
+                <Marker
+                    coordinate={region}
+                    title={address?.city ?? 'Unknown'}
+                    description={`${address?.street ?? 'Unknown'} ${address?.streetNumber ?? ''}`}
+                />
+            </MapView>
+        </SafeAreaView>
+    );
 }
-
-const styles = StyleSheet.create({
-    SafeAreaView: {
-        flex: 1,
-        backgroundColor: '#fff',
-    },
-});
