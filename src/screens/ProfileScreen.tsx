e95b6296319dc580bd95c1a2d0a3a9f26c03270a
Anthony Bouillant <Anthony.Bouillant@cpnv.ch>
2022-06-25 17:26:38 +0200

    :sparkles: Create delete tracks

--- src/screens/ProfileScreen.tsx
+++ src/screens/ProfileScreen.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Alert } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import {
     Avatar,
@@ -11,64 +11,79 @@ import {
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import { AuthContext } from '../navigation/AuthProvider';
 import config from '../config/config.json';
+import axios from 'axios';
 
-export default class ProfileScreen extends Component {
-    static contextType = AuthContext;
-    constructor(props: any) {
-        super(props);
+export default function ProfileScreen() {
+    const context = React.useContext(AuthContext);
+
+
+    const deleteTracking = async () => {
+        await axios.delete(config.server + '/api/rfc/location?', {
+            headers: {
+                Authorization: `Bearer ${context.user.token}`
+            }
+        }).catch(error => {
+            console.log(error);
+        });
+
     }
 
-    render() {
-        return (
-            <SafeAreaView style={styles.SafeAreaView}>
-                <View style={styles.container}>
-                    <View style={styles.userInfoSection}>
-                        <View style={{ flexDirection: 'row', marginTop: 15 }}>
-                            <Avatar.Image
-                                source={{
-                                    uri: config.server + config.apiUrlUserPicture + this.context.user.picture,
-                                }}
-                                size={100}
-                            />
-                            <View style={{ marginLeft: 20 }}>
-                                <Title style={[styles.title, {
-                                    marginTop: 15,
-                                    marginBottom: 5,
-                                }]}>{this.context.user.name}</Title>
-                            </View>
-                        </View>
-                    </View>
+    const showAlert = () =>
+        Alert.alert(
+            "Confirmation de suppression",
+            "Etes-vous sûr de vouloir supprimer votre historique de navigation ?",
+            [
+                {
+                    text: "Annuler",
+                    style: "cancel",
+                },
+                { text: "Confirmer", onPress: deleteTracking },
+            ],
+        );
 
-                    <View style={styles.userInfoSection}>
-                        <View style={styles.row}>
-                            <Icon name="email" color="#777777" size={20} />
-                            <Text style={{ color: "#777777", marginLeft: 20 }}>{this.context.user.email}</Text>
+    return (
+        <SafeAreaView style={styles.SafeAreaView}>
+            <View style={styles.container}>
+                <View style={styles.userInfoSection}>
+                    <View style={{ flexDirection: 'column', marginTop: 15 }}>
+                        <Avatar.Image
+                            source={{
+                                uri: config.server + '/storage/pics/' + context.user.picture,
+                            }}
+                            size={100}
+                        />
+                        <View>
+                            <Title style={[styles.title, {
+                                marginTop: 15,
+                                marginBottom: 5,
+                            }]}>
+                                {context.user.name}
+                            </Title>
                         </View>
-                        <View style={styles.row}>
-                            <Icon name="phone" color="#777777" size={20} />
-                            <Text style={{ color: "#777777", marginLeft: 20 }}>{this.context.user.phone}</Text>
-                        </View>
-                        {
-                            this.context.user.description != null ? (
-                                <View style={styles.row}>
-                                    <Icon name="comment-text-outline" color="#777777" size={20} />
-                                    <Text style={{ color: "#777777", marginLeft: 20 }}>{this.context.user.description}</Text>
-                                </View>
-                            ) : null
-                        }
-                    </View>
-                    <View style={styles.menuWrapper}>
-                        <TouchableRipple onPress={() => { }}>
-                            <View style={styles.menuItem}>
-                                <Icon name="cog-outline" color="#FF6347" size={25} />
-                                <Text style={styles.menuItemText}>Settings</Text>
-                            </View>
-                        </TouchableRipple>
                     </View>
                 </View>
-            </SafeAreaView>
-        );
-    }
+                <View style={styles.userInfoSection}>
+                    <View style={styles.row}>
+                        <Icon name="email" color="#777777" size={20} />
+                        <Text style={{ color: "#777777", marginLeft: 20 }}>{context.user.email}</Text>
+                    </View>
+                    <View style={styles.row}>
+                        <Icon name="phone" color="#777777" size={20} />
+                        <Text style={{ color: "#777777", marginLeft: 20 }}>{context.user.phone}</Text>
+                    </View>
+                </View>
+                <View style={styles.menuWrapper}>
+                    <TouchableRipple onPress={showAlert}>
+                        <View style={styles.menuItem}>
+                            <Icon name="trash-can-outline" color="#FF6347" size={25} />
+                            <Text style={styles.menuItemText}>Supprimer les traces</Text>
+                        </View>
+                    </TouchableRipple>
+                </View>
+            </View>
+        </SafeAreaView>
+    );
+
 };
 
 const styles = StyleSheet.create({
